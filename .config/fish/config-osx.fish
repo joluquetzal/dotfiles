8b5b58d2cb1ff6b6f51e2731a355d3d1f70da947
joluquetzal <jluissalber@gmail.com>
2023-04-17 02:45:51 +0200

    adds new keymaps

--- .config/fish/config-osx.fish
+++ .config/fish/config-osx.fish
@@ -3,4 +3,6 @@
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
+  alias ll2 "ll --tree --level=2"
+  alias ll3 "ll --tree --level=3"
 end
--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,5 +1,8 @@
 local keymap = vim.keymap
 
+-- Set space as leader key
+vim.g.mapleader = " "
+
 -- Do not yank with x
 keymap.set('n', 'x', '"_x')
 
@@ -8,13 +11,15 @@ keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
 -- Select all
-keymap.set('n', 'll', 'gg<S-v>G')
+keymap.set('n', 'lg', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 
@@ -23,3 +28,16 @@ keymap.set('n', '<C-w><left>', '<C-w>>')
 keymap.set('n', '<C-w><right>', '<C-w><')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- Moving selected chunks
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Rename at position
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
